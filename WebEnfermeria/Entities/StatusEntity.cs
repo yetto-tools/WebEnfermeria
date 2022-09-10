2bd70ba98ec7cf934255c621a29356fec40e2d4d
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-09-10 12:46:40 -0600

    Add Entity Status

---
+++ WebEnfermeria/Entities/StatusEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebEnfermeria.Entities.Status
+{
+    public class StatusEmployeeEntity
+    {
+        public Guid Id { get; set; }
+        [StringLength(maximumLength: 20, ErrorMessage = "Field Maximum Length is {0}")]
+        public string Status { get; set; }
+    }
+    public class StatusEntity
+    {
+        public Guid Id { get; set; }
+        [StringLength(maximumLength: 20, ErrorMessage = "Field Maximum Length is {0}")]
+        public string Status { get; set; }
+    }
+}
