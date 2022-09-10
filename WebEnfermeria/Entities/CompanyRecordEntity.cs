4f4dc613e4d94eaa7b5845536b7ce5960d90aeb1
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-09-10 12:45:35 -0600

    Add Entity Record Company Employee

---
+++ WebEnfermeria/Entities/CompanyRecordEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebEnfermeria.Entities.Company;
+
+namespace WebEnfermeria.Entities.CompaniesRecord
+{
+    public class CompanyRecordEntity
+    {
+        public UInt32 Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fecha de Ingreso")]
+        public DateTime Income { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fecha de Egreso")]
+        public DateTime? Egress { get; set; }
+
+        public CompanyEntity CompanyId { get; set; }
+
+    }
+}
