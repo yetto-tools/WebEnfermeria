422f49039163e33493ba99729d5ff64dad4ce05b
Erick Orlando EORG. Rashon Gonzalez <PAMERG01@gpamericana.com>
2022-09-10 12:44:51 -0600

    Add Entity Company

---
+++ WebEnfermeria/DTOs/CompanyRecordDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebEnfermeria.DTOs.Company;
+using WebEnfermeria.DTOs.Employee;
+
+namespace WebEnfermeria.DTOs.CompaniesRecord
+{
+    public class CompaniesRecordDTO
+    {
+        public UInt32 Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fecha de Egreso")]
+
+        public DateTime Income { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fecha de Egreso")]
+
+        public DateTime? Egress { get; set; }
+
+        //public EmployeeDTO Employee { get; set; }
+        public CompanyDTO CompaniesId { get; set; } 
+    }
+}
---
+++ WebEnfermeria/DTOs/StatusDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebEnfermeria.DTOs.Status
+{
+    public class StatusEmployeeDTO
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "Field Maximum Length is {0} and Minimum Length is {1}")]
+        public string Status { get; set; }
+
+    }
+}
